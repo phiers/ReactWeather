1ec469ba3f515430d55709fac78addbddc93fc53
Paul Hiers <paulhiers@yahoo.com>
2016-09-28 12:46:45 -0400

    Fix localhost issue

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
-// redirect all https traffic to http (so openWeatherMap api will work)
+//redirect all https traffic to http (so openWeatherMap api will work)
 app.use(function(req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next(); //the next() function just processes the request
-  } else {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next(); //the next() function just processes the request
   }
 });
 //set app to use Public folders
